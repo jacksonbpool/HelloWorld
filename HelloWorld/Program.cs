e158e23e5aa36c902877cbb12fd30e4e460d6bc6
jacksonbpool <jacksonbpoole@gmail.com>
2014-10-16 13:32:08 +0200

    second

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world again!");
+            Console.WriteLine("Hello world!");
         }
     }
 }
